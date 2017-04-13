1a2d99fcceaa3a8764692c5640f331c03bb97541
sungatullina <esungatullina@gmail.com>
2017-04-13 12:41:07 +0300

    Запуск  тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,23 +1,39 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Sungatullina on 05.04.2017.
  */
 public class ApplicationManager {
-    FirefoxDriver wd;
+    WebDriver wd;
 
   private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (Objects.equals(browser, BrowserType.FIREFOX)){
+      wd = new FirefoxDriver();
+    } else if (Objects.equals(browser, BrowserType.CHROME)){
+      wd = new ChromeDriver();
+    } else if (Objects.equals(browser, BrowserType.IE)){
+      wd = new InternetExplorerDriver();
+    }
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
---
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/Equality.java
@@ -0,0 +1,16 @@
+package ru.stqa.ptf.sandbox;
+
+/**
+ * Created by Sungatullina on 13.04.2017.
+ */
+public class Equality {
+
+  public static void main(String[] args){
+    String s1= "firefox 2.0";
+    String s2= "firefox " + Math.sqrt(4.0);
+
+    System.out.println(s1 == s2);         //сравнение ссылок
+    System.out.println(s1.equals(s2));    //сравнение содержимого объекта
+  }
+}
+
