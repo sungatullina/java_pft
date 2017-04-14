ebd5877f3b9529b2910fda869f55ae3fda6a84fc
sungatullina <esungatullina@gmail.com>
2017-04-14 12:34:40 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -14,6 +14,11 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
@@ -23,6 +28,9 @@ public class NavigationHelper extends HelperBase{
   }
 
   public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
       click(By.linkText("home"));
       wd.get("http://localhost/addressbook/");
   }
