5b8fcc9334f2d9b7a1d0d55a7d36c685c1853787
sungatullina <esungatullina@gmail.com>
2017-04-20 16:03:43 +0300

    Сортировка списков

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -28,9 +29,11 @@ public class ContactCreationTests extends TestBase{
     }*/
 
 
-    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Comparator<? super ContactData> byId = (c1 , c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -23,7 +24,7 @@ public class ContactModificationTests extends TestBase {
     app.getNavigationHelper().gotoHomePage();
     app.getContactHelper().selectContact(before.size() -1);
     app.getContactHelper().initContactModification();
-    ContactData contact = new ContactData(before.get(before.size() -1).getId(), "Александр1", "Пучков", "Москва2", "88008888", "test@test.ru", null);
+    ContactData contact = new ContactData(before.get(before.size() -1).getId(), "Александр", "Пучков", "Москва2", "88008888", "test@test.ru", null);
     app.getContactHelper().fillContactForm(contact,false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().gotoHomePage();
@@ -32,7 +33,10 @@ public class ContactModificationTests extends TestBase {
 
     before.remove(before.size()-1);
     before.add(contact);
-    Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+    Comparator<? super ContactData> byId = (c1 , c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
 
 
   }
