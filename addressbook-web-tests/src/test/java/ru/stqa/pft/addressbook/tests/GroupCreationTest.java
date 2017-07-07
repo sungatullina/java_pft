406529db216aae61318688b0a20684f217a75611
sungatullina <esungatullina@gmail.com>
2017-07-07 14:26:08 +0300

    6.5 Загрузка тестовых данных из файла
    Провайдер тестовых данных загружает информацию/тестовые данные из
    внешнего файла, который был сгенерирован генератором тестовых данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,11 +17,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTest extends TestBase{
 
   @DataProvider
-  public Iterator<Object[]> validGroups(){
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
-    list.add(new Object[]{new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
-    list.add(new Object[]{new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
@@ -34,10 +39,10 @@ public class GroupCreationTest extends TestBase{
       Groups after = app.group().all(); //список элементов, после того как создана новая группа
       assertThat(after, equalTo(
               before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
-
  }
 
-  @Test
+
+  @Test (enabled = false)
   public void testBadGroupCreation() {
     app.goTo().groupPage();
     Groups before = app.group().all();
@@ -48,5 +53,4 @@ public class GroupCreationTest extends TestBase{
     Groups after = app.group().all(); //список элементов, после того как создана новая группа
     assertThat(after, equalTo(before));
   }
-
 }
