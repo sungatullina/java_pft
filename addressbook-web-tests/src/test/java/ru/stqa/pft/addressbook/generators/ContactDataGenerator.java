420665583ab9e656ec45e5b436f093972bf91933
sungatullina <esungatullina@gmail.com>
2017-07-11 13:59:05 +0300

    6.7 Формат JSON and Ex.14 Реализовать генераотры тестовых данных
    Реализованы генераторы тестовых данных для групп и для контактов,
    которые сохраняют данные в файл в формате XML или JSON

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -0,0 +1,85 @@
+package ru.stqa.pft.addressbook.generators;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Пользователь on 10.07.2017.
+ */
+public class ContactDataGenerator {
+
+    @Parameter(names = "-c", description = "Group count")
+    public int count;
+
+    @Parameter (names = "-f", description = "Target file")
+    public String file;
+
+    @Parameter (names = "-d", description = "Data format")
+    public String format;
+
+    public static void main(String[] args) throws IOException {
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts (count);
+        if (format.equals("xml")){
+            saveAsXml(contacts, new File(file));
+        } else if (format.equals("json")){
+            saveAsJson(contacts, new File(file));
+        } else {
+            System.out.println("Unrecognized format" + format);
+        }
+    }
+
+    private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+        String json = gson.toJson(contacts);
+        Writer writer = new FileWriter(file);
+        writer.write(json);
+        writer.close();
+
+    }
+
+    private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        String xml = xstream.toXML(contacts);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+    }
+
+
+
+    private List<ContactData> generateContacts(int count) {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        for (int i = 0; i<count; i++) {
+            contacts.add(new ContactData().withFirstname(String.format("Alex %s", i))
+                    .withLastname(String.format("Pychkov %s", i)).withAddress(String.format("Moscow %s", i)).withHomePhone(String.format("8800888800 %s", i))
+                    .withMobilePhone(String.format("+79159999815 %s", i)).withWorkPhone(String.format("(495)101010 %s", i)).withEmail(String.format("test@test.ru %s", i))
+                    .withEmail2(String.format("test2@test.ru %s", i)).withEmail3(String.format("test3@test.ru %s", i)).withGroup(String.format("test1 %s", i)));
+        }
+        return contacts;
+    }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,18 +1,35 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 
+@XStreamAlias("contact")
+
 public class ContactData {
+  @XStreamOmitField
   private  int id = Integer.MAX_VALUE;
+  @Expose
   private String firstname;
+  @Expose
   private String lastname;
+  @Expose
   private String address;
+  @Expose
   private String homePhone;
+  @Expose
   private String mobilePhone;
+  @Expose
   private String workPhone;
+  @Expose
   private String email;
+  @Expose
   private String email2;
+  @Expose
   private String email3;
+  @Expose
   private String group;
   private String allPhones;
   private String allEmail;
