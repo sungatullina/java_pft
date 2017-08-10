8e1e3e9e7bf34a8806a913ce8f52c24055d77ee7
sungatullina <esungatullina@gmail.com>
2017-08-10 18:35:37 +0300

    9.3 SOAP. Взаимодействие с MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -1,12 +1,15 @@
 package ru.stqa.pft.mantis.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
 import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * Created by Пользователь on 10.08.2017.
@@ -20,5 +23,13 @@ public class SoapTests extends TestBase{
         for (Project project : projects){
             System.out.println(project.getName());
         }
+    }
+
+    @Test
+    public void testCreateIssue() throws MalformedURLException, ServiceException, RemoteException{
+        Set<Project> projects = app.soap().getProjects();
+        Issue issue = new Issue().withSummary("Test issue").withDescription("Test issue description").withProject(projects.iterator().next());
+        Issue created = app.soap().addIssue(issue);
+        assertEquals(issue.getSummary(),created.getSummary());
     }
 }
